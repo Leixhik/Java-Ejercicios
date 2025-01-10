40a1036e44c7321a3047c942e74d39a227eae0be
Rodolfo Leija <leija_26010@hotmail.com>
2025-01-09 18:09:40 -0600

    v.0.0.2.
    He reformateado la estructura del proyecto, además de empezar con los ejercicios de la sección de Variables de Udemy.

---
+++ EjerciciosJava/src/variables/IntroduccionALasVariables/Ejercicios.java
@@ -0,0 +1,67 @@
+package variables.IntroduccionALasVariables;
+
+public class Ejercicios {
+    public static void main(String[] args) {
+        ejercicio7();
+    }
+
+    public static void ejercicio1(){
+        /* 1. Declaración simple: Declara una variable de tipo entero llamada
+        edad y asígnale tu edad. */
+
+        int edad = 24;
+    }
+
+    public static void ejercicio2(){
+        /* 2. Declaración múltiple: Declara tres variables de tipo String:
+        nombre, apellido y ciudad, en una sola línea.*/
+        
+        String nombre, apellido, ciudad;
+    }
+
+    public static void ejercicio3(){
+        /* 3. Inicialización: Declara una variable de tipo double llamada precio
+         e inicialízala con el valor 19.99*/
+
+        double precio = 19.99;
+    }
+
+    public static void ejercicio4(){
+        /* 4. Constantes: Declara una constante de tipo int llamada DIAS_SEMANA
+        con el valor 7.*/
+
+        int DIAS_SEMANA = 7;
+    }
+
+    public static void ejercicio5(){
+        /* 5. Tipos mixtos: Declara una variable nombreCompleto concatenando variables
+        nombre y apellido.*/
+
+        String nombre = "Rodolfo", apellido = "Leija";
+        String nombreCompleto = nombre + " " + apellido;
+        System.out.println(nombreCompleto);
+    }
+
+    public static void ejercicio6(){
+        /* 6. Ejercicio de conversión implícita: Declara un int y asígnale un valor.
+         Luego, asígnale ese valor a un double. Imprime ambos valores. */
+
+        int valor1 = 10;
+        double valor2 = valor1;
+        System.out.println(valor1 + " // " + valor2);
+    }
+
+    public static void ejercicio7(){
+        /* 7. Ejercicio de conversión explícita (casting): Declara un double con
+        un valor decimal. Luego, conviértelo a un int e imprime el resultado.*/
+
+        double valor1 = 23.3;
+        int valorDecimal = (int)valor1;
+        System.out.println(valor1 + " // " + valorDecimal);
+    }
+
+    public static void ejercicio8(){
+
+    }
+
+}
